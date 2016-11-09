38a63eb922ac0619d864494caca09a85d8742132
ouweiming <ouweiming@levoson.cn>
2016-11-09 18:09:20 +0800

    add GlobalProperty file, record static field;

---
+++ app/src/main/java/com/owm/translation/utils/GlobalProperty.java
@@ -0,0 +1,13 @@
+package com.owm.translation.utils;
+
+/**
+ * Created by ouweiming on 2016/11/9.
+ */
+
+public class GlobalProperty {
+
+    public static final int TRANSLATION_TYPE_BAIDU = 0x1;
+    public static final int TRANSLATION_TYPE_YOUDAO = 0x2;
+
+
+}
